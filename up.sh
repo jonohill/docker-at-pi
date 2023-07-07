65fa292f6c4ced656f817654759f0ce169224207
Jono Hill <jono@hillnz.com>
2023-07-08 10:51:22 +1200

    Disable a few things

--- up.sh
+++ up.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-git pull
+# git pull
 mkdir -p data
-SOPS_AGE_KEY_FILE=key.txt sops exec-env secrets.enc.env 'docker-compose up -d tailscale' || true
+# SOPS_AGE_KEY_FILE=key.txt sops exec-env secrets.enc.env 'docker-compose up -d tailscale' || true
 SOPS_AGE_KEY_FILE=key.txt sops exec-env secrets.enc.env 'docker-compose up --remove-orphans -d'
